420c9415e2831889fd97ead3c2adf303fa164f2c
Simone Guzzi <simone.guzzi@sib.swiss>
2025-01-04 12:21:46 +0100

    build: serve openapi.json under /api endpoint

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_THREAD_ID = os.getenv("OPENAI_THREAD_ID")
 OPENAI_ASSISTANT_ID = os.getenv("OPENAI_ASSISTANT_ID")
 
-app = FastAPI(docs_url="/api/docs/", redoc_url="/api/redoc/")
+app = FastAPI(
+    docs_url="/api/docs/", redoc_url="/api/redoc/", openapi_url="/api/openapi.json"
+)
 
 app.add_middleware(
     CORSMiddleware,
